009a16a028d99c84b058e7d8a2452d9e9ecacd40
Michal Jez <Michal.G.Jez@gmail.com>
2021-05-08 16:49:55 -0400

    Setup apollo and some page scaffolding

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { Route, Switch } from "react-router";
+import React from "react";
+import { Home } from "./Home";
+
+export const Pages = () => (
+  <Switch>
+    {/* These routes are semi-placeholder and subject to change */}
+    <Route path="/" render={() => <Home />} />
+
+    <Route path="/room/:roomGuid" />
+
+    <Route path="/dashboard" />
+    <Route path="/dashboard/controller" />
+    <Route path="/dashboard/rooms" />
+    <Route path="/dashboard/account" />
+
+    <Route path="/help-center" />
+    <Route path="/help-center/:helpCenterArticleGuid" />
+
+    <Route render={() => null} />
+  </Switch>
+);
