a94f2fecc685dd624f76c44808e5e83f4deac2bb
brzhuang <robbie.zhuang@gmail.com>
2021-06-08 18:50:04 -0400

    Remove blog from navbar

--- src/pages/Navbar/navbar.tsx
+++ src/pages/Navbar/navbar.tsx
@@ -8,16 +8,18 @@ import {
 } from "@chakra-ui/react";
 
 export const Navbar = () => (
-  <Flex maxWidth="1000px" margin="0 auto" padding="40px 20px 0 20px">
+  <Flex
+    alignItems="center"
+    maxWidth="1000px"
+    margin="0 auto"
+    padding="40px 20px 0 20px"
+  >
     <Box>
       <Heading size="3xl">V</Heading>
     </Box>
     <Spacer />
     <Box>
       <ButtonGroup>
-        <Button color="white" colorScheme="whitealpha" variant="ghost">
-          Blog
-        </Button>
         <Button color="white" colorScheme="whitealpha" variant="ghost">
           Sign in
         </Button>
