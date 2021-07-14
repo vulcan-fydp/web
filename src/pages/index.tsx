0305d43ba6efce547b079ac2461f1b64f4aea284
Robbie Zhuang <robbie.zhuang@gmail.com>
2021-07-14 04:11:12 -0400

    TEST PR: Dashboard with routers 2 (#8)
    * Have a Dashboard component with nested switches
    
    * Fix routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Route, Switch } from "react-router";
 import { Home } from "./Home";
-import { Dashboard } from "pages/room/Room";
 import { LoginRouter } from "pages/login";
 import { RoomRouter } from "./room";
 
@@ -11,11 +10,6 @@ export const Pages = () => (
 
     <Route path="/room" render={() => <RoomRouter />} />
     <Route path="/login" render={() => <LoginRouter />} />
-
-    <Route path="/dashboard" render={() => <Dashboard />} />
-    <Route path="/dashboard/controller" />
-    <Route path="/dashboard/rooms" />
-    <Route path="/dashboard/account" />
 
     <Route path="/help-center" />
     <Route path="/help-center/:helpCenterArticleGuid" />
