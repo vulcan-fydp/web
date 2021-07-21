82a53f318f36740ac6548f50cf85eda04b2e0a19
Robbie Zhuang <robbie.zhuang@gmail.com>
2021-07-21 16:37:45 -0400

    Add basics

--- src/pages/room/Room/streamTab.tsx
+++ src/pages/room/Room/streamTab.tsx
@@ -1,14 +1,47 @@
 import {
   VStack,
-  Heading,
 } from "@chakra-ui/react";
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { ApolloClient, InMemoryCache, gql, FetchResult, HttpLink } from '@apollo/client/core';
+import { SubscriptionClient } from "subscriptions-transport-ws";
+
+// TODO: Get these dynamically (from backend?)
+const controlAddress = "https://localhost:9443";
+const signalAddress = "wss://localhost:8443";
+
+let clientSub: SubscriptionClient | null = null;
 
 export const StreamTab: React.FC = () => {
+
+  const stream = <video controls></video>;
+
   return (
-    <>
-      <VStack alignItems="center" spacing="20px">
-        <video></video>
-      </VStack>
-    </>
+    <VStack alignItems="center" spacing="20px">
+      {stream}
+    </VStack>
   );
+}
+
+function getControlConnection() {
+  const httpLink = new HttpLink({
+      uri: controlAddress,
+  });
+  return new ApolloClient({
+      link: httpLink,
+      cache: new InMemoryCache(),
+  })
+}
+
+function getSignalConnection(token: string) {
+  let sub = new SubscriptionClient(signalAddress, {
+      connectionParams: {
+          token
+      }
+  });
+  const wsLink = new WebSocketLink(sub);
+  let client = new ApolloClient({
+      link: wsLink,
+      cache: new InMemoryCache(),
+  })
+  return { client, sub };
 }
