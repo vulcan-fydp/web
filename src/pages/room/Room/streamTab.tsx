09456b7e829207ba74eb7490c86b0e2039e989be
Robbie Zhuang <robbie.zhuang@gmail.com>
2021-07-21 11:56:08 -0400

    Install packages

---
+++ src/pages/room/Room/streamTab.tsx
@@ -0,0 +1,14 @@
+import {
+  VStack,
+  Heading,
+} from "@chakra-ui/react";
+
+export const StreamTab: React.FC = () => {
+  return (
+    <>
+      <Heading size="md">Connected Players</Heading>
+      <VStack alignItems="left" spacing="20px">
+      </VStack>
+    </>
+  );
+}
