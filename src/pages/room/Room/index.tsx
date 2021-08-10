4b105705c43932e207429c3cda77e1c1e3ba5a73
Callum Moseley <callum.moseley1@gmail.com>
2021-08-10 12:41:57 -0500

    add https to room link

--- src/pages/room/Room/index.tsx
+++ src/pages/room/Room/index.tsx
@@ -97,7 +97,7 @@ const RoomDetails = () => {
             rightIcon={<Image src={copy}></Image>}
             justifyContent="space-between"
             onClick={() => {
-              navigator.clipboard.writeText(room_url);
+              navigator.clipboard.writeText("https://" + room_url);
               setTooltipShowing(true);
             }}
           >
