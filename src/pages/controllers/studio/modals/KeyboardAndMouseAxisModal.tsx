f9e14c294ab9f3c77bcca82848bc818d68c63a04
Michal Jez <Michal.G.Jez@gmail.com>
2021-11-26 16:04:39 -0500

    Add keyboard axis customization

--- src/pages/controllers/studio/modals/KeyboardAndMouseAxisModal.tsx
+++ src/pages/controllers/studio/modals/KeyboardAndMouseAxisModal.tsx
@@ -10,38 +10,77 @@ import {
   Text,
   Button,
   HStack,
+  Flex,
 } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import { AxisModalComponent } from "./AxisModal";
+import { KeyboardAxisModal } from "./KeyboardAxisModal";
+
+enum AxisType {
+  KEYBOARD,
+  MOUSE,
+}
 
 export const KeyboardAndMouseAxisModal: AxisModalComponent = ({
   isOpen,
   onAxisChange,
   onClose,
 }) => {
+  const [axisType, setAxisType] = useState<AxisType>();
+
+  useEffect(() => {
+    setAxisType(undefined);
+  }, [isOpen, setAxisType]);
+
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <KeyboardAxisModal
+        isOpen={isOpen && axisType === AxisType.KEYBOARD}
+        onAxisChange={onAxisChange}
+        onClose={onClose}
+      />
+      <Modal isOpen={isOpen && axisType === undefined} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Choose an axis type</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <HStack
-              borderColor="purple.200"
-              borderStyle="dashed"
-              borderWidth="5px"
-              w="100%"
-              h="120px"
-              borderRadius="4px"
-            >
-              <Button>Keyboard</Button>
-              <Button>Mouse</Button>
-            </HStack>
+            <Flex w="100%" h="120px" justifyContent="space-between">
+              <Button
+                borderColor="purple.200"
+                borderStyle="dashed"
+                borderWidth="5px"
+                borderRadius="4px"
+                h="100%"
+                flex="1"
+                variant="outline"
+                marginRight="20px"
+                onClick={() => setAxisType(AxisType.KEYBOARD)}
+              >
+                ⌨️ Keyboard
+              </Button>
+              <Button
+                borderColor="purple.200"
+                borderStyle="dashed"
+                borderWidth="5px"
+                borderRadius="4px"
+                h="100%"
+                flex="1"
+                variant="outline"
+                onClick={() => setAxisType(AxisType.MOUSE)}
+              >
+                🖱️ Mouse
+              </Button>
+            </Flex>
           </ModalBody>
           <ModalFooter>
-            <Button w="100%" variant="solid" size="sm">
-              Unset Button
+            <Button
+              w="100%"
+              variant="solid"
+              size="sm"
+              onClick={() => onAxisChange(null)}
+            >
+              Unset Axis
             </Button>
           </ModalFooter>
         </ModalContent>
---
+++ src/styles/thickDashedBorder.ts
@@ -0,0 +1,6 @@
+export const thickDashedBorder = {
+  borderColor: "purple.200",
+  borderStyle: "dashed",
+  borderWidth: "5px",
+  borderRadius: "4px",
+} as const;
