f3bedf02837792707294a169e6c72863980098d8
Michal Jez <Michal.G.Jez@gmail.com>
2022-01-21 11:02:13 -0500

    Add snippet file for docs

---
+++ src/static/pages/docs/content/docs/vulcast/ControllerPassthrough.doc.tsx
@@ -0,0 +1,12 @@
+import { defineDoc } from "static/pages/docs/components/defineDoc";
+
+defineDoc({
+  title: "Controller Passthrough",
+  content: (
+    <>
+      You can use your Vulcast to pass controller inputs from non-Switch
+      controllers to your Switch simply by plugging them in the the Vulcas via
+      USB!
+    </>
+  ),
+});
