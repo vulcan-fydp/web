38aaabf08103d72526fe1e16566fe850c67aa6a3
Michal Jez <Michal.G.Jez@gmail.com>
2022-01-22 17:32:43 -0500

    Add scripts to watch codegenable files (#149)
    * Add scripts to watch codegenable files
    
    * Fix build

--- src/static/pages/docs/content/docs/deploying/Aws.doc.tsx
+++ src/static/pages/docs/content/docs/deploying/Aws.doc.tsx
@@ -2,5 +2,5 @@ import { defineDoc } from "static/pages/docs/components/defineDoc";
 
 defineDoc({
   title: "Deploying to Amazon Web Services",
-  content: <>Hello</>,
+  content: <>Hello AWS!</>,
 });
