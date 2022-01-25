81eca953619b98d68e346202fd3d14a58439afbb
Michal Jez <Michal.G.Jez@gmail.com>
2022-01-24 20:49:38 -0500

    Refactor navbar to be a reusable component across sites + banner

--- src/app/components/HeroPage/index.tsx
+++ src/app/components/HeroPage/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Navbar } from "app/components/Navbar";
+import { DefaultNavbar } from "app/components/Navbar";
 import { Flex, Box } from "@chakra-ui/layout";
+import { useNavbarHeight } from "lib/Navbar";
 
 interface HeroProps {
   isDashboard?: boolean;
 }
+
+const HeroPageContent: React.FC = ({ children }) => {
+  const navbarHeight = useNavbarHeight();
+
+  return (
+    <Flex
+      pt={`${navbarHeight + 30}px`}
+      minHeight="100vh"
+      flexDir="column"
+      align="center"
+    >
+      {children}
+    </Flex>
+  );
+};
 
 export const HeroPage: React.FC<HeroProps> = ({ children, isDashboard }) => {
   return (
@@ -15,10 +31,9 @@ export const HeroPage: React.FC<HeroProps> = ({ children, isDashboard }) => {
           : "linear-gradient(132.85deg, #30292F 0%, #413F54 100%);"
       }
     >
-      <Navbar />
-      <Flex pt="80px" minHeight="100vh" flexDir="column" align="center">
-        {children}
-      </Flex>
+      <DefaultNavbar>
+        <HeroPageContent>{children}</HeroPageContent>
+      </DefaultNavbar>
     </Box>
   );
 };
