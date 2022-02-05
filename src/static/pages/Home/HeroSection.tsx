04eac4cf9913ee2b15df6d9f36e51fd9e3ea7810
Michal Jez <Michal.G.Jez@gmail.com>
2022-02-05 15:30:09 -0500

    Begin adding homepage hero section

--- src/static/pages/Home/HeroSection.tsx
+++ src/static/pages/Home/HeroSection.tsx
@@ -1,1 +1,129 @@
-export const HeroSection = () => null;
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import SkylineImg from "static/resources/skyline-2.png";
+import TelevisionImgSrc from "static/resources/hero-tv.jpg";
+import DeskImgSrc from "static/resources/hero-desk.jpg";
+import BedroomImgSrc from "static/resources/hero-bedroom.jpg";
+import CouchImgSrc from "static/resources/hero-couch.jpg";
+import TestScreenImgSrc from "static/resources/test-screen.png";
+
+const TelevisionImage = () => (
+  <Box
+    borderRadius="10px"
+    borderColor="yellow.300"
+    borderWidth="4px"
+    position="relative"
+  >
+    <Image src={TelevisionImgSrc} borderRadius="4px" />
+    <Box
+      position="absolute"
+      top="81px"
+      left="114px"
+      right="112px"
+      objectFit="contain"
+    >
+      <Image src={TestScreenImgSrc} objectFit="contain" />
+    </Box>
+  </Box>
+);
+
+const DeskImage = () => (
+  <Box
+    borderRadius="10px"
+    borderColor="yellow.300"
+    borderWidth="4px"
+    position="relative"
+  >
+    <Image src={DeskImgSrc} borderRadius="4px" />
+    <Box
+      position="absolute"
+      top="22px"
+      left="205px"
+      right="62px"
+      objectFit="contain"
+    >
+      <Image src={TestScreenImgSrc} objectFit="contain" />
+    </Box>
+  </Box>
+);
+
+const BedroomImage = () => (
+  <Box
+    borderRadius="10px"
+    borderColor="yellow.300"
+    borderWidth="4px"
+    position="relative"
+  >
+    <Image src={BedroomImgSrc} borderRadius="4px" />
+    <Box
+      position="absolute"
+      top="159px"
+      left="139px"
+      right="203px"
+      objectFit="contain"
+    >
+      <Image src={TestScreenImgSrc} objectFit="contain" />
+    </Box>
+  </Box>
+);
+
+const CouchImage = () => (
+  <Box
+    borderRadius="10px"
+    borderColor="yellow.300"
+    borderWidth="4px"
+    position="relative"
+  >
+    <Image src={CouchImgSrc} borderRadius="4px" />
+    <Box
+      position="absolute"
+      top="165px"
+      left="154px"
+      right="191px"
+      objectFit="contain"
+    >
+      <Image src={TestScreenImgSrc} objectFit="contain" />
+    </Box>
+  </Box>
+);
+
+export const HeroSection: React.FC = () => {
+  return (
+    <Box width="100vw" height="100vh" overflow="hidden" position="relative">
+      <Box position="absolute" top="20px" left="20px">
+        <Text
+          fontSize={{ base: "4xl", md: "6xl" }}
+          color="purple.400"
+          fontWeight="bold"
+        >
+          Vulcan Gaming
+        </Text>
+        <Text>
+          Bring your console to your friends! &nbsp;
+          <Button mt="10px" variant="link">
+            Get Started!
+          </Button>
+        </Text>
+      </Box>
+      <Box
+        position="absolute"
+        top="50px"
+        right="200px"
+        left="calc(100vw - 400px - 200px)"
+        bottom="calc(100vh - 300px - 50px)"
+      >
+        <CouchImage />
+      </Box>
+      <Box
+        position="absolute"
+        bottom="0"
+        left="0"
+        right="0"
+        top="calc(100vh - 300px)"
+        backgroundImage={`url(${SkylineImg})`}
+        backgroundSize="contain"
+        backgroundRepeat="repeat-x"
+        backgroundPosition="bottom"
+      ></Box>
+    </Box>
+  );
+};
