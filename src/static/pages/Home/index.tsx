0fc50b568ecc24aa771641946c7eaabb08c0f24b
Michal Jez <Michal.G.Jez@gmail.com>
2022-02-10 19:18:21 -0500

    Add about section (#170)
    * Add about section
    
    * Update src/static/pages/Home/AboutSection.tsx
    
    Co-authored-by: Robbie Zhuang <robbie.zhuang@gmail.com>
    
    Co-authored-by: Robbie Zhuang <robbie.zhuang@gmail.com>

--- src/static/pages/Home/index.tsx
+++ src/static/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import { Footer } from "static/components/Footer";
 import { HeroSection } from "./HeroSection";
 import { FeaturesSection } from "./FeaturesSection";
 import { SetUpSection } from "./SetUpSection";
+import { AboutSection } from "./AboutSection";
 
 export const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <AboutSection />
       <FeaturesSection />
       <SetUpSection />
       <Footer />
