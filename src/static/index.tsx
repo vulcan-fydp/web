14bd37657b0268169435de50f7e2a02ea6899200
brzhuang <robbie.zhuang@gmail.com>
2022-03-25 17:37:43 -0400

    Add font to static site

--- src/static/index.tsx
+++ src/static/index.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { vulcanTheme } from "theme";
 import { Router } from "./pages";
+import "@fontsource/montserrat/800.css";
 
 const VulcanGamingPlatformStaticSite = () => {
   return (
